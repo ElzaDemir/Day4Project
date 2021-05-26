4e42fbab656fccda92e1d786a803ab75bf3858b0
ElviraDemir <ismayilova.elvira@gmail.com>
2021-05-25 20:18:23 -0500

    TC 001 has been completed

--- src/test/java/TestCase001.java
+++ src/test/java/TestCase001.java
@@ -1,4 +1,7 @@
 public class TestCase001 {
     //Work 1
     //Work2
+    //ready to push to remote
+    //ready to open pull request (permission to merge)
+    //discuss and eventually merge the code
 }
